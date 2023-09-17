9465e3b91c6c4d9d7ef0b4d70998cca0395469ce
svea001 <142218157+svea001@users.noreply.github.com>
2023-09-17 10:01:50 +0200

    Update script2.js

--- static/script2.js
+++ static/script2.js
@@ -1,10 +1,6 @@
-// JavaScript code (add this to your HTML file or a separate .js file)
 document.addEventListener("DOMContentLoaded", function () {
-    // Get references to the buttons and response text element
     const readyButton = document.getElementById("submit1");
     const responseText = document.getElementById("response-text");
-
-    // Add click event listeners to the buttons
     readyButton.addEventListener("click", function () {
         responseText.textContent = "Cool! Now fill out the mood questions!";
     });
